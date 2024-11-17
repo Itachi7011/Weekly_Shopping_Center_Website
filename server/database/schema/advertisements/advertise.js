f9057ec02a9dc24e994bb7a1030af7e49fe98fce
Shivam <coolsan929@gmail.com>
2024-11-17 21:52:36 +0530

    commit

--- server/database/schema/advertisements/advertise.js
+++ server/database/schema/advertisements/advertise.js
@@ -2,15 +2,25 @@ const mongoose = require("mongoose");
 const { buffer } = require("stream/consumers");
 
 const W_Mark_Market_Page_Specific_Advetises = new mongoose.Schema({
-    title: {
+    sponserName: {
         type: String,
     },
     content: {
         type: String,
     },
-    iconClass: {
+    position: [{
         type: String,
-    },
+    }],
+    categories: [{
+        type: String,
+    }],
+
+    subCategories: [{
+        type: String,
+    }],
+    tags: [{
+        type: String,
+    }],
     logoImage:
     {
         data: String,
