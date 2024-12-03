9974889ba489a1587290c413d99f739fe3667094
Shivam <coolsan929@gmail.com>
2024-12-03 16:39:41 +0530

    commit

--- client/src/Components/Products/ProductsList.jsx
+++ client/src/Components/Products/ProductsList.jsx
@@ -561,7 +561,7 @@ const ProductListing = () => {
 
 
 
-                                <img src={product.images[0].data} className="card-img-top" alt={product.name} style={{ width: "100%", height: "450px", objectFit: "cover", position: "relative", cursor: "pointer" }} onClick={function () {
+                                <img src={product.images[0].data} className="card-img-top" alt={product.name} style={{ width: "100%", height: "450px", objectFit: "cover", position: "relative", cursor: "pointer", padding:"0.3rem" }} onClick={function () {
                                     const formattedName = encodeURIComponent(product.name);
                                     console.log("Navigating to ProductProfile with name:", formattedName);
                                     navigate(`/ProductProfile/${product.name}`, {
